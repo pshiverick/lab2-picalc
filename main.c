be6f25775768b2adfbdaeffaa5fa04f159b05156
Parker Shiverick <parker@Parker-Shiverick.local>
2017-11-11 19:11:39 -0500

    Lab 2 Final Code

--- main.c
+++ main.c
@@ -20,8 +20,7 @@ void cleanup();
 #define DEVICE_NAME_LEN 128
 static char dev_name[DEVICE_NAME_LEN];
 
-#define NUM_ITEMS 12
-#define debugSize 5
+#define NUM_ITEMS 4096
 
 int main()
 {
@@ -42,13 +41,19 @@ int main()
 
 		float final_sum = 0.0;
 		float pi = 0.0;
-		const int num_wg = 3;
-//		const int num_wi = 2;
-		int denominators[NUM_ITEMS] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23};
-		float *result = malloc(sizeof(float)*num_wg);
+		const int num_wg = 16;
+		const int num_wi = 16;
+		int denominators[NUM_ITEMS];
 		int i = 0;
-		int num_vars_in_wi = 2;
-		float *debug = malloc(sizeof(float)*debugSize);
+		int value = 1;
+
+		/* Populate array of values for denominators */
+		for(i = 0; i < NUM_ITEMS; i++){
+			denominators[i] = value;
+			value += 2;
+		}
+	 
+		float *result = malloc(sizeof(float)*num_wg);
 
 #ifdef __APPLE__
     /* Get Platform and Device Info */
@@ -89,7 +94,6 @@ int main()
 
     /* Create Command Queue */
     command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-
 #ifdef __APPLE__
     /* Load the source code containing the kernel*/
     fp = fopen(fileName, "r");
@@ -136,41 +140,31 @@ int main()
       exit(1);
     }
 
+		/* Create Buffers for I/O */
 		cl_mem bufferDen = clCreateBuffer(context, CL_MEM_READ_ONLY,
 				sizeof(int)*NUM_ITEMS, NULL, &ret);
 	  clEnqueueWriteBuffer(command_queue, bufferDen, CL_TRUE, 0,
 				sizeof(int)*NUM_ITEMS, (void *) denominators, 0, NULL, NULL);
-
 		
-		cl_mem debugBuf = clCreateBuffer(context, CL_MEM_READ_WRITE,
-				sizeof(float)*debugSize, NULL, &ret);
 		cl_mem resultBuf = clCreateBuffer(context, CL_MEM_READ_WRITE, 
 				sizeof(float)*num_wg, NULL, &ret);
 
     /* Execute the kernel */
-		size_t globalws = 2;
-		size_t localws = 2;
+		size_t globalws = (size_t)(num_wg*num_wi);
+		size_t localws = (size_t)num_wi;
 
     /* Set the kernel arguments */
+		/* Pass in Denominators */
     ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&bufferDen);
-/*		if(ret != CL_SUCCESS) 
+		/* Define size of local memory */
+		ret |= clSetKernelArg(kernel, 1, sizeof(float)*num_wi, NULL);
+		/* Pass in result buffer */
+		ret |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&resultBuf);
+		if(ret != CL_SUCCESS)
 		{
-			printf("invalid argument: %d", 0);
+			printf("Set kernel arguments failed\n");
 			printf("return value: %d", ret);
-		}*/
-		ret = clSetKernelArg(kernel, 1, sizeof(float)*localws, NULL);
-/*		if(ret != CL_SUCCESS)
-		{
-			printf("invalid argument: %d", 0);
-			printf("return value: %d", ret);
-		} */
-		ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&resultBuf);
-/*		if(ret != CL_SUCCESS)
-		{
-			printf("invalid argument: %d", 0);
-			printf("return value: %d", ret);
-		}*/
-		ret = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *)&debugBuf);
+		}
 
     ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
       &globalws, &localws, 0, NULL, NULL);
@@ -183,33 +177,26 @@ int main()
       exit(1);
     }
 
+		/* Read results back -- array of floats that we must add up
+		 * to get the final result */
 		clEnqueueReadBuffer(command_queue, resultBuf, CL_TRUE, 0, 
 				sizeof(float)*num_wg, (void *)result, 0, NULL, NULL);
-		clEnqueueReadBuffer(command_queue, debugBuf, CL_TRUE, 0,
-				sizeof(float)*debugSize, (void *)debug, 0, NULL, NULL);
 		
+		/* Add up result buffer and multiply by four to get the final result */
+		final_sum = 0.0;
 		for(i = 0; i < num_wg; i++)
 		{
-			printf("result[%d] = %f\n", i, result[i]);
 			final_sum += result[i];
 		}
 		pi = final_sum*4.0;
 
+		/* Print out final result of pi that we calculated */
 		printf("pi = %f\n", pi);
 
-    /* Copy the output data back to the host */
-//    clEnqueueReadBuffer(command_queue, bufferD, CL_TRUE, 0, wD*hD*sizeof(float),
-//         (void *)D, 0, NULL, NULL);
-
-    /* free resources */
-//    free(D);
 		free(result);
-		free(debug);
 
 		clReleaseMemObject(resultBuf);
 		clReleaseMemObject(bufferDen);
-		clReleaseMemObject(debugBuf);
-//    clReleaseMemObject(bufferA);
     clReleaseCommandQueue(command_queue);
     clReleaseKernel(kernel);
     clReleaseProgram(program);
--- mykernel.cl
+++ mykernel.cl
@@ -1,47 +1,56 @@
 __kernel void calculatePi(
     __global int *bufferDen,
 		__local float *sumBuf,
-		__global float *resultBuf,
-		__global float *debug
+		__global float *resultBuf
 		)
 {
-	/* initialize memory items */
-		sumBuf[0] = 0;
-		sumBuf[1] = 0;
-
-		resultBuf[0] = 0;
-		resultBuf[1] = 0;
-
-		barrier(CLK_GLOBAL_MEM_FENCE);
-		
-	  const int num_vars = 2;
-		float values[num_vars];
+	/* Execute 16 calculations per work item */
+		const int num_vars_per_wi = 16;
+		float values[num_vars_per_wi];
 		int i = 0;
 		int index = 0;
+		float wi_sum = 0;
+		float local_sum = 0;
 
-    int gid = get_global_id(0); /* work group 	*/
+		/* Need the following values to find the index of the
+			 * denominator that we need to find */
+    int gid = get_group_id(0); /* work group 	*/
 		int lid = get_local_id(0);  /* work item 		*/
 		int wi_size = get_local_size(0); /* # work items */
-
-		for(i = 0; i < num_vars; i++)
+		
+		/* Extract array of denominators that we will work on */
+		for(i = 0; i < num_vars_per_wi; i++)
 		{
-			index = gid*num_vars*wi_size + wi_size*lid + i;
+			index = gid*num_vars_per_wi*wi_size + wi_size*lid + i;
 			values[i] = (float) bufferDen[index];
 		}
 
-		sumBuf[lid] = 1.0/values[0];
-		for(i = 1; i < num_vars; i++)
-		{
-			sumBuf[lid] -= 1.0/values[i];
+		/* Add up 1/X - 1/Y + 1/Z - 1/A. etc... */
+		float small_sum = 0;
+		index = 0;
+		for(i = 0; i < num_vars_per_wi/2; i++){
+			small_sum = (float)1/values[index] - (float)1/values[index+1];
+			wi_sum += small_sum;
+			index += 2;
 		}
 
+		/* Add final sum of work item to local memory */
+		sumBuf[lid] = wi_sum;
+
+		/* Wait for all work items in work group to finish processing */
 		barrier(CLK_LOCAL_MEM_FENCE);
 
+		/* Work item 0 will add up all of the sums of the work items in each
+			 * work group */
 		if(lid == 0)
 		{
 			for(i = 0; i < wi_size; i++) 
 			{
-				resultBuf[gid] += sumBuf[i];
+				local_sum += sumBuf[i];
 			}
+
+			/* Add final result to output buffer. This value is the result
+				 * of all the work items operations. */
+			resultBuf[gid] = local_sum;
 		}
 }
